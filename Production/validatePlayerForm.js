aa03c3f6cbc3fd30ab234c0c8edd4b521bce0971
Aidan B <46637527+ab-n3xt@users.noreply.github.com>
2020-11-20 11:05:14 -0500

    Created new Players table in MySQL; added/removed input accordingly

--- Production/validatePlayerForm.js
+++ Production/validatePlayerForm.js
@@ -1,15 +1,19 @@
-function validateForm(playerInGame, playerName, playerDescription, playerSettings, playerLink, playerImage, playerTags) {
+function validateForm(playerInGameName, playerName, playerCountry, playerDOB, playerDesc, playerSettings, playerImage, playerTags) {
     let playerInGameValidation = /^[0-9a-zA-Z]+$/;
     let nameValidation = /^[a-zA-Z\s]+$/;
-    if (playerInGame.value.length == 0 || playerName.value.length == 0) {
+    if (playerInGameName.value.length == 0 || playerName.value.length == 0) {
         alert("In-Game and Actual Name can't be 0 characters long!");
         return false;
     }
-    if (playerInGame.value.length > 30 || playerName.value.length > 30) {
-        alert("In-Game and Actual Name must be less than 30 characters long!");
+    if (playerInGameName.value.length > 30) {
+        alert("In-Game Name must be 30 characters long or less!");
         return false;
     }
-    if (!playerInGame.value.match(playerInGameValidation)) {
+    if (playerName.value.length > 40) {
+        alert("Full Name must be 40 characters long or less!");
+        return false;
+    }
+    if (!playerInGameName.value.match(playerInGameValidation)) {
         alert("In-Game name must consist of only letters and numbers!");
         return false;
     }
@@ -17,20 +21,20 @@ function validateForm(playerInGame, playerName, playerDescription, playerSetting
         alert("Actual name must consist of only letters and spaces!");
         return false;
     }
-    if (playerDescription.value.length > 3000) {
-        alert("The description can only be a max of 3000 characters!");
+    if (!playerCountry.value.match(nameValidation)) {
+        alert("Country must consist of only letters and spaces!");
         return false;
     }
-    if (playerSettings.value.length > 1000) {
-        alert("The setting can only be a max of 1000 cahracters!");
+    if (playerDesc.value.length > 5000) {
+        alert("The description can only be a max of 5000 characters!");
         return false;
     }
-    if (playerLink.value.length > 300) {
-        alert("The link can only be a max of 300 characters!");
+    if (playerSettings.value.length > 500) {
+        alert("The setting can only be a max of 500 cahracters!");
         return false;
     }
-    if (playerImage.value.length > 300) {
-        alert("The player image can only be a max of 300 characters");
+    if (playerImage.value.length > 100) {
+        alert("The player image can only be a max of 100 characters");
         return false;
     }
     if (playerTags.value.length > 300) {
