f43ab0ba61e5ad6c608bc1d3096f3f3f3ad72eec
Aidan B <46637527+ab-n3xt@users.noreply.github.com>
2020-11-21 01:59:46 -0500

    File change: addTeam.html -> addTeam.php

--- Production/addTeam.php
+++ Production/addTeam.php
@@ -38,7 +38,7 @@
 </header>
 <main class="container-md">
     <h1>Submit a Team Profile!</h1>
-    <form action="addToTeam.php"
+    <form action="addTeamDB.php"
           enctype="multipart/form-data"
           method="post"
           onsubmit="return validateForm(
@@ -48,20 +48,56 @@
               )
     ">
         <div class="form-group">
-            <label for="teamName">Team Name</label>
-            <input type="text" class="form-control" id="teamName" name="teamName">
+            <label for="Game">Select Game</label>
+            <select class="form-control" id="Game" name="Game">
+                <?php
+                include("connect.php");
+                $sql = "SELECT GameID, Title FROM Games";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo "<option value='".$row['GameID']."'>".$row['Title']."</option>";
+                    }
+                } else {
+                    echo "0 results";
+                }
+                $conn->close();
+                ?>
+            </select>
         </div>
         <div class="form-group">
-            <label for="teamDesc">Description</label>
-            <textarea class="form-control" id="teamDesc" rows="5" name="teamDesc" style="white-space: pre-line;"></textarea>
+            <label for="Sponsor">Select Sponsor</label>
+            <select class="form-control" id="Sponsor" name="Sponsor">
+                <?php
+                include("connect.php");
+                $sql = "SELECT SponsorID, Name FROM Sponsors";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo "<option value='".$row['SponsorID']."'>".$row['Name']."</option>";
+                    }
+                } else {
+                    echo "0 results";
+                }
+                $conn->close();
+                ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="teamName">Team Name</label>
+            <input type="text" class="form-control" id="teamName" name="teamName">
         </div>
         <div class="form-group">
             <label for="teamImage">Image</label>
             <input type="file" onchange="ValidateSize(this)" class="form-control" id="teamImage" name="teamImage">
         </div>
         <div class="form-group">
-            <label for="playerTags">Tags</label>
-            <input type="text" class="form-control" id="playerTags" name="playerTags">
+            <label for="teamDesc">Description</label>
+            <textarea class="form-control" id="teamDesc" rows="5" name="teamDesc" style="white-space: pre-line;"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="teamTags">Tags</label>
+            <input type="text" class="form-control" id="teamTags" name="teamTags">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
