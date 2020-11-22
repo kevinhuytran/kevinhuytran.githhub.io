2f69afe6fcf72020ac09f5d260b7874b10bf2094
Aidan B <46637527+ab-n3xt@users.noreply.github.com>
2020-11-22 15:19:40 -0500

    Updated header so they all have proper links

--- Production/playerForm.php
+++ Production/playerForm.php
@@ -16,21 +16,32 @@
 </head>
 <body>
 <header>
-    <nav class="navbar navbar-expand-lg navbar-dark mb-5" style="background-color: #2B303A;">
-        <a class="navbar-brand" href="#">Navbar</a>
+    <nav class="navbar navbar-expand-md navbar-dark mb-5" style="background-color: #2B303A;">
+        <a class="navbar-brand" href="."><img src="images/logo2.svg" style="max-width: 50px"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Games</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Admin
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="gameForm.html">Add Game</a>
+                        <a class="dropdown-item" href=".">Add Player</a>
+                        <a class="dropdown-item" href="sponsorForm.html">Add Sponsor</a>
+                        <a class="dropdown-item" href="teamForm.php">Add Team</a>
+                    </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Players</a>
+                    <a class="nav-link" href="listGames.php">Games</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Teams</a>
+                    <a class="nav-link" href="listPlayers.php">Players</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="listTeams.php">Teams</a>
                 </li>
             </ul>
         </div>
