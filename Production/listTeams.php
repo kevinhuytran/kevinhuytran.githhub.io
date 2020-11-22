9d3a4d49c0557cca03d50dfdcc18e07222fbfb56
Aidan B <46637527+ab-n3xt@users.noreply.github.com>
2020-11-21 23:02:20 -0500

    Added JQuery transforms to images

--- Production/listTeams.php
+++ Production/listTeams.php
@@ -7,6 +7,17 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function(){
+            $("img").hover(function(){
+                $(this).css("transition", "transform .3s");
+
+                $(this).css("transform", "scale(1.15)");
+            }, function(){
+                $(this).css("transform", "scale(1)");
+            });
+        });
+    </script>
     <style>
         header {
             font-family: "DejaVu Sans Mono", sans-serif;
@@ -49,15 +60,17 @@
 </header>
 <main class="container-md">
     <h2>Teams</h2>
-    <div class="list-group">
+    <div class="row">
         <?php
         include("connect.php");
-        $sql = "SELECT TeamID, TeamName FROM Teams";
+        $sql = "SELECT TeamID, TeamName, ImagePath FROM Teams";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             // output data of each row
             while ($row = $result->fetch_assoc()) {
-                echo "<a href='viewTeam.php?TeamID=" . $row['TeamID'] . "' class='list-group-item list-group-item-action'>" . $row['TeamName'] . "</a>";
+                echo "<div class='col-md-4'><div class='thumbnail'>";
+                echo "<a href='viewTeam.php?TeamID=" . $row['TeamID'] . "' target='_blank'><img src='" . $row['ImagePath'] . "' alt='TeamPhoto' style='width:100%'" . "</a>";
+                echo "</div></div>";
             }
         } else {
             echo "0 results";
