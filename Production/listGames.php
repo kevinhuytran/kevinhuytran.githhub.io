a508a6264dc4c6545ccc7bfe4f42a22894e92789
Aidan B <46637527+ab-n3xt@users.noreply.github.com>
2020-11-23 00:23:50 -0500

    Fixed img transformations

--- Production/listGames.php
+++ Production/listGames.php
@@ -9,7 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <script>
         $(document).ready(function(){
-            $("img").hover(function(){
+            $("main img").hover(function(){
                 $(this).css("transition", "transform .3s");
                 $(this).css("transform", "scale(1.15)");
             }, function(){
