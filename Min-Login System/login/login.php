1d508a85e1fba728023811f0a42311e77f681b25
Minseung27 <50151546+Minseung27@users.noreply.github.com>
2020-12-02 02:57:18 +0900

    login/signup Page

---
+++ Min-Login System/login/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+echo "Username: ".$username. "<br>";
+echo "Password: ".$password. "<br>";
+?>
+</body>
+
+</html>
