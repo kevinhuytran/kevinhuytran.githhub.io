20c4567f3b64d1d8d6a615ce5c11f926dde67beb
Aidan B <46637527+ab-n3xt@users.noreply.github.com>
2020-12-02 14:56:21 -0500

    Initial file

---
+++ Production/editSponsor.php
@@ -0,0 +1,103 @@
+<?php
+require("connect.php");
+$sponsorID = $_GET['SponsorID'];
+$sql = "SELECT * FROM Sponsors WHERE SponsorID ='" . $sponsorID . "'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$conn->close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Esport Encyclopedia</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <style>
+        header {
+            font-family: "DejaVu Sans Mono", sans-serif;
+        }
+    </style>
+    <script src="validateSponsorForm.js"></script>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark mb-5" style="background-color: #2B303A;">
+        <a class="navbar-brand" href="."><img src="images/logo2.svg" style="max-width: 50px"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Admin
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="gameForm.html">Add Game</a>
+                        <a class="dropdown-item" href="playerForm.php">Add Player</a>
+                        <a class="dropdown-item" href=".">Add Sponsor</a>
+                        <a class="dropdown-item" href="teamForm.php">Add Team</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="listGames.php">Games</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="listPlayers.php">Players</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="listSponsors.php">Sponsors</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="listTeams.php">Teams</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<main class="container-md">
+    <h1>Submit a Team Profile!</h1>
+    <form action="updateSponsor.php?SponsorID=<?php echo $sponsorID; ?>"
+          enctype="multipart/form-data"
+          method="post"
+          onsubmit="return validateForm(
+              document.getElementById('sponsorName'),
+              document.getElementById('sponsorDesc'),
+              document.getElementById('sponsorTags')
+              )
+    ">
+        <div class="form-group">
+            <label for="sponsorName">Sponsor Name</label>
+            <input type="text" class="form-control" id="sponsorName" name="sponsorName" value="<?php echo $row['Name']; ?>">
+        </div>
+        <div class="form-group">
+            <label for="sponsorImage">Image</label>
+            <input type="file" onchange="ValidateSize(this)" class="form-control" id="sponsorImage" name="sponsorImage">
+        </div>
+        <div class="form-group">
+            <label for="sponsorIcon">Icon</label>
+            <input type="file" onchange="ValidateSize(this)" class="form-control" id="sponsorIcon" name="sponsorIcon">
+        </div>
+        <div class="form-group">
+            <label for="sponsorDesc">Description</label>
+            <textarea class="form-control" id="sponsorDesc" rows="5" name="sponsorDesc" style="white-space: pre-line;">
+                <?php
+                $sponsorDesc = $row['SponsorDesc'];
+                $breaks = array("<br />","<br>","<br/>");
+                $sponsorDesc = str_ireplace($breaks, "\r\n", $sponsorDesc);
+                echo $sponsorDesc;
+                ?>
+            </textarea>
+        </div>
+        <div class="form-group">
+            <label for="sponsorTags">Tags</label>
+            <input type="text" class="form-control" id="sponsorTags" name="sponsorTags" value="<?php echo $row['Tags']; ?>">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</main>
+</body>
+</html>
