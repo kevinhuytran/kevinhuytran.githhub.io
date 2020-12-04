8cb950691f53fdf27da4f849d6eba1b30e932da2
Minseung27 <50151546+Minseung27@users.noreply.github.com>
2020-12-05 07:16:13 +0900

    session created (working)
    session 'username' created!

--- Min-Login System/login/login.php
+++ Min-Login System/login/login.php
@@ -1,6 +1,8 @@
 <?php
 require("connect.php");
 
+session_start();
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 
@@ -9,6 +11,10 @@ $result = $conn->query($sql);
 
 //login success. redirecting to main page
 if ($result->num_rows > 0){
+
+    //session username created
+    $_SESSION['username']=$username;
+    
     header("Location: http://3.230.1.132/index.html");
 }
 
