b67866c1e1acc6584d8f53d4fc361ad190241723
Aidan B <46637527+ab-n3xt@users.noreply.github.com>
2020-12-05 12:16:13 -0500

    Ported header to header.php; removed redundant code

--- Production/index.php
+++ Production/index.php
@@ -22,53 +22,7 @@ include('login.php');
     </style>
 </head>
 <body>
-<header>
-    <nav class="navbar navbar-expand-md navbar-dark mb-5" style="background-color: #2B303A;">
-        <a class="navbar-brand" href="."><img src="images/logo2.svg" style="max-width: 50px"></a>
-        <a class="navbar-brand" href="aboutUs.html">ABOUT US</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                    <?php
-                    if(isset($_SESSION['id'])) {
-                        if ($_SESSION['id'] == 1) {
-                            echo '<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Admin
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="gameForm.html">Add Game</a>
-                                        <a class="dropdown-item" href="playerForm.php">Add Player</a>
-                                        <a class="dropdown-item" href="sponsorForm.html">Add Sponsor</a>
-                                        <a class="dropdown-item" href="teamForm.php">Add Team</a>
-                                    </div>
-                            ';
-                        }
-                    }
-                    ?>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="listGames.php">Games</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="listPlayers.php">Players</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="listSponsors.php">Sponsors</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="listTeams.php">Teams</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</header>
-<?php
-echo $_SESSION['username'];
-echo $_SESSION['id'];
-?>
+<?php include('header.php'); ?>
 <main class="container-md">
     <div class="container-fluid px-md-5">
         <img class="img-fluid mx-auto d-block" src="images/ee-logo.png">
