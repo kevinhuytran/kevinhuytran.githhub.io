57a1a499a98dcbda3bfe471fbeabc66b4c89e54f
Aidan B <46637527+ab-n3xt@users.noreply.github.com>
2020-12-05 12:11:52 -0500

    Login/Session

--- Production/login.php
+++ Production/login.php
@@ -4,12 +4,13 @@ require("connect.php");
 if(isset($_POST['login'])){
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $sql = "SELECT * FROM Users WHERE Username='$username' AND Password='$password'";
+    $sql = "SELECT UserID, Username, Password FROM Users WHERE Username='$username' AND Password='$password'";
     $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
 
     if ($result->num_rows > 0){
         $_SESSION['username'] = $username; //kt
-        $_SESSION['id'] = $result['UserID'];
+        $_SESSION['id'] = $row['UserID'];
         header("Location: http://3.230.1.132/index.php");
     }
 
